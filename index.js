d3e86cfdf83a03e2fff6ffd79b9ebd38525dcd21
mark teng <mark_19475@hotmail.com>
2016-06-10 10:40:26 +0700

    initial react with express server

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use('/src', express.static(__dirname + '/src'));
+app.use('/build', express.static(__dirname + '/build'));
+
+app.use('/', function(req, res) {
+	res.sendFile(__dirname + '/src/index.html');
+});
+
+app.listen(3000, function() {
+	console.log('listening on *:3000');
+}); 
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+ 
+module.exports = {
+    
+    entry: path.resolve(__dirname, 'src/main.js'),
+ 
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+ 
+    module: {
+        loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }
+        ]
+    }
+};
